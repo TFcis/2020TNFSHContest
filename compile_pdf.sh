ac0a3d354c2ad17b0db009acf27ef88bba8f6db9
xiplus <huangxuanyuxiplus@gmail.com>
2020-02-08 18:46:18 +0800

    Create compile_pdf.sh

---
+++ compile_pdf.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Usage: ./compile_pdf.sh [problem-id]
+
+contestName="2020臺南一中校內賽"
+
+cd "$1"
+
+echo "Compiling $1"
+
+pandoc -M contestName="$contestName" -T "" problem.md -o problem.pdf --template ../template.tex -f markdown -t latex -s --latex-engine=xelatex
+
+echo "Compile Finish"
