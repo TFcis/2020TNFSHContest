45a8f7b609dc371e590242ebf33f0655bcddd2e6
xiplus <huangxuanyuxiplus@gmail.com>
2020-02-09 21:12:50 +0800

    Update statement.sh

--- Route/scripts/statement.sh
+++ Route/scripts/statement.sh
@@ -1,10 +1,10 @@
-CONTEST_NAME="$(python3 "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "contest_name")"
-PROBLEM_LABEL="$(python3 "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "problem_label")"
-PROBLEM_NAME="$(python3 "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "title")"
-PROBLEM_ID="$(python3 "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "name")"
+CONTEST_NAME="$(python "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "contest_name")"
+PROBLEM_LABEL="$(python "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "problem_label")"
+PROBLEM_NAME="$(python "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "title")"
+PROBLEM_ID="$(python "${INTERNALS}/json_extract.py" "${PROBLEM_JSON}" "name")"
 
 echo "Compiling ${CONTEST_NAME} - ${PROBLEM_LABEL}. ${PROBLEM_NAME} (${PROBLEM_ID})"
 
-pandoc -M contestName="${CONTEST_NAME}" -M problemLabel="${PROBLEM_LABEL}" -M problemName="${PROBLEM_NAME}" -M problemId="${PROBLEM_ID}" -T "" ${STATEMENT_DIR}/problem.md -o ${STATEMENT_DIR}/problem.pdf --template ${BASE_DIR}/../template.tex -f markdown -t latex -s --latex-engine=xelatex
+pandoc -M contestName="${CONTEST_NAME}" -M problemLabel="${PROBLEM_LABEL}" -M problemName="${PROBLEM_NAME}" -M problemId="${PROBLEM_ID}" -T "" ${STATEMENT_DIR}/problem.md -o ${STATEMENT_DIR}/problem.pdf --template ${BASE_DIR}/../template.tex -f markdown -t latex -s --pdf-engine=xelatex
 
 echo "Compile Finish"
--- compile_pdf.sh
+++ compile_pdf.sh
@@ -8,6 +8,6 @@ cd "$1"
 
 echo "Compiling $1"
 
-pandoc -M contestName="$contestName" -T "" problem.md -o problem.pdf --template ../template.tex -f markdown -t latex -s --latex-engine=xelatex
+pandoc -M contestName="$contestName" -T "" problem.md -o problem.pdf --template ../template.tex -f markdown -t latex -s --pdf-engine=xelatex
 
 echo "Compile Finish"
