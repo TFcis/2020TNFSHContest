6d72cbc90f4afa0969b5d886ca283dc713115ce3
xiplus <huangxuanyuxiplus@gmail.com>
2020-02-11 19:43:14 +0800

    Matching按照測資範圍重產，並根據solutions產出輸出，新增validator

---
+++ Matching/validator/validator.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+	registerValidation();
+
+	int N = inf.readInt();
+	inf.readEoln();
+
+	for (int q = 0; q < N; q++) {
+		inf.readInt(1, N);
+		inf.readSpace();
+		inf.readInt(1, N);
+		inf.readEoln();
+	}
+
+	inf.readEof();
+
+	return 0;
+}
