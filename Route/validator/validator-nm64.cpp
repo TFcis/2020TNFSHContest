dac2b6841f00a9ffacdfda9b610a89fd74de53d7
xiplus <huangxuanyuxiplus@gmail.com>
2020-02-11 18:52:57 +0800

    Route加入測資範圍檢查程式

---
+++ Route/validator/validator-nm64.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+	registerValidation();
+
+	int N = inf.readInt(1, 1000);
+	inf.readSpace();
+	int M = inf.readInt(1, 1000);
+	inf.readEoln();
+
+	ensure(N <= 64);  // task 4
+	ensure(M <= 64);  // task 4
+
+	char format[100];
+	sprintf(format, "[0-9]{%d}", M);
+
+	for (int q = 0; q < N; q++) {
+		inf.readToken(format);
+		inf.readEoln();
+	}
+
+	inf.readEof();
+
+	return 0;
+}
---
+++ Route/validator/validator.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+	registerValidation();
+
+	int N = inf.readInt(1, 1000);
+	inf.readSpace();
+	int M = inf.readInt(1, 1000);
+	inf.readEoln();
+
+	char format[100];
+	sprintf(format, "[0-9]{%d}", M);
+
+	for (int q = 0; q < N; q++) {
+		inf.readToken(format);
+		inf.readEoln();
+	}
+
+	inf.readEof();
+
+	return 0;
+}
